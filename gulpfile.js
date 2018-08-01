14d98a17f3dea8daf1e9072bce822b0ab16cd8b2
LeeVaughn <dlvprogramming@gmail.com>
2018-08-01 10:47:28 -0700

    added function to convert SCSS files to CSS, then concat and minify them

--- gulpfile.js
+++ gulpfile.js
@@ -3,13 +3,24 @@
 const gulp = require("gulp");
 const concat = require("gulp-concat");
 const uglify = require("gulp-uglify");
+const sass = require("gulp-sass");
+const clean = require("gulp-clean-css");
 
 // concatenates and minifies JavaScript files
 gulp.task("scripts", function () {
     gulp.src(["js/circle/autogrow.js", "js/circle/circle.js", "js/global.js"])
-    .pipe(concat("all.js"))
+    .pipe(concat("all.min.js"))
     .pipe(uglify())
     .pipe(gulp.dest("dist/scripts"));
+});
+
+// compliles SCSS files into CSS, then concatenates and minifies them
+gulp.task("styles", function () {
+    gulp.src("sass/global.scss")
+    .pipe(sass())
+    .pipe(concat("all.min.css"))
+    .pipe(clean())
+    .pipe(gulp.dest("dist/styles"));
 });
 
 gulp.task("default", ["build"], function () {
