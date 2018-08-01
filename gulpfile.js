9832f255177956d4fcce139e9a5356645b1051b9
LeeVaughn <dlvprogramming@gmail.com>
2018-07-31 19:38:11 -0700

    required gulp-concat and gulp-uglify and added function to concat and minify JS files

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,17 @@
 "use strict";
 
 const gulp = require("gulp");
+const concat = require("gulp-concat");
+const uglify = require("gulp-uglify");
+
+// concatenates and minifies JavaScript files
+gulp.task("scripts", function () {
+    gulp.src(["js/circle/autogrow.js", "js/circle/circle.js", "js/global.js"])
+    .pipe(concat("all.js"))
+    .pipe(uglify())
+    .pipe(gulp.dest("dist/scripts"));
+});
+
+gulp.task("default", ["build"], function () {
+    // will be used to run gulp build task and serve project to local host
+});
