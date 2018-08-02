790d8db8748a113dff1a4e2e06c77afb2940a2d0
LeeVaughn <dlvprogramming@gmail.com>
2018-08-02 01:02:07 -0700

    added run-sequence and gulp-webserver packages, added tasks to move html file and icons folder to dist folder, finished build and default tasks

--- gulpfile.js
+++ gulpfile.js
@@ -8,14 +8,15 @@ const sass = require("gulp-sass");
 const cleanCSS = require("gulp-clean-css");
 const imageMin = require("gulp-imagemin");
 const clean = require("del");
+const runSequence = require("run-sequence");
+const webserver = require("gulp-webserver");
 const options = {
     src: "src",
     dist: "dist"
 };
 
-// concatenates and minifies JavaScript files
+// creates source map for JavaScript files, concatenates and minifies them and saves the file to the dist folder
 gulp.task("scripts", function () {
-    // return gulp.src(["js/circle/autogrow.js", "js/circle/circle.js", "js/global.js"])
     return gulp.src([options.src + "/js/**"])
         .pipe(maps.init())
         .pipe(concat("all.min.js"))
@@ -24,7 +25,7 @@ gulp.task("scripts", function () {
         .pipe(gulp.dest(options.dist + "/scripts"));
 });
 
-// compliles SCSS files into CSS, then concatenates and minifies them
+// creates source map for SCSS files, compliles them to CSS, concat/minifies them and saves the file to the dist folder
 gulp.task("styles", function () {
     return gulp.src(options.src + "/sass/global.scss")
         .pipe(maps.init())
@@ -35,15 +36,28 @@ gulp.task("styles", function () {
         .pipe(gulp.dest(options.dist + "/styles"));
 });
 
+// optimizes images then saves them to the dist folder
 gulp.task("images", function () {
     return gulp.src(options.src + "/images/**")
         .pipe(imageMin())
         .pipe(gulp.dest(options.dist + "/content"));
 });
 
+// copies index.html to dist folder
+gulp.task("html", function () {
+    return gulp.src(options.src + "/index.html")
+        .pipe(gulp.dest("dist"));
+});
+
+// copies icons folder to dist folder
+gulp.task("icons", function () {
+    return gulp.src(options.src + "/icons/**", { base: "./src"})
+        .pipe(gulp.dest("dist"));
+});
+
 // watches for changes to Sass files and then runs the gulp styles command
 gulp.task("watchSass", function () {
-    gulp.watch(options.src + "/sass/**", ["styles"]);
+    return gulp.watch(options.src + "/sass/**", ["styles"])
 });
 
 // removes previously created dist directory and contents
@@ -51,11 +65,23 @@ gulp.task("clean", function () {
     return clean("dist");
 });
 
-// gulp build command will run scripts and styles
-gulp.task("build", ["scripts", "styles"], function () {
-
+// runs clean task first, then runs other tasks in parallel
+gulp.task("build", function () {
+    return runSequence("clean", ["scripts", "styles", "images", "html", "icons"]);
 });
 
-gulp.task("default", ["build"], function () {
-    // will be used to run gulp build task and serve project to local host
+// creates webserver on port 300 with live reload, uses build task as a dependency
+gulp.task("webserver", function () {
+    return gulp.src("dist")
+        .pipe(webserver({
+            port: 3000,
+            livereload: true,
+            open: true
+        }));
+});
+
+// starts webserver, uses watchSass task as a dependency
+gulp.task("default", ["watchSass"], function () {
+    // runSequence("clean", ["scripts", "styles", "images", "html", "icons"], "webserver");
+    runSequence(["build"], "webserver");
 });
